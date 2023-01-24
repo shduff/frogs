8006cc6c90d2989fc36937893e7e479fd6f4e45f
Shaunalynn Duffy <shduff@Shaunalynns-MBP.hsd1.ma.comcast.net>
2023-01-24 11:23:49 -0500

    connected site to Airtable info, organized state saving around reload and reset

--- script.js
+++ script.js
@@ -1,5 +1,175 @@
-// Whenever the page loads
-window.addEventListener("load", (e) => {
+// Whenever the page loads, first fetch data from Airtable
+function fetchData(callback) {
+	let quizInfo = [];
+	let journalInfo = [];
+	let journalPrompts = [];
+	let frogFacts = [];
+
+	// Whenever the page loads
+	window.addEventListener("load", (e) => {
+		// Connect to Airtable to get all the quiz/journal info
+		let Airtable = require('airtable');
+		Airtable.configure({
+		    endpointUrl: 'https://api.airtable.com',
+		    apiKey: 'keycuL2P2p7WPUAZz'
+		});
+		let base = Airtable.base('appEhPPxg2z51MAtm');
+
+		// Grab all the quiz info and save it into quizInfo
+		let fetchQuizInfo = new Promise(resolve => {
+			let numQuizQuestions = 10;
+			base('quiz').select({
+			    // Selecting the first 3 records in Grid view:
+			    maxRecords: numQuizQuestions,
+			    view: "Grid view"
+			}).eachPage(function page(records, fetchNextPage) {
+			    // This function (`page`) will get called for each page of records.
+
+			    records.forEach(function(record) {
+			        quizInfo.push({
+						"question": record.get('Question'),		        
+						"answer1": record.get('Answer 1'),
+						"frog1": record.get('Frog 1'),
+				        "answer2": record.get('Answer 2'),
+						"frog2": record.get('Frog 2'),
+				        "answer3": record.get('Answer 3'),
+						"frog3": record.get('Frog 3'),
+				        "answer4": record.get('Answer 4'),
+						"frog4": record.get('Frog 4'),
+				        "answer5": record.get('Answer 5'),
+						"frog5": record.get('Frog 5'),
+				        "answer6": record.get('Answer 6'),
+						"frog6": record.get('Frog 6'),
+			        });
+			    });
+
+			    // To fetch the next page of records, call `fetchNextPage`.
+			    // If there are more records, `page` will get called again.
+			    // If there are no more records, `done` will get called.
+			    fetchNextPage();
+
+			}, function done(err) {
+			    if (err) { 
+			    	console.error(err); return; 
+			    } else {
+			        resolve(quizInfo);
+			    }
+			});
+		});
+
+		// Grab all the journal info and save it into journalInfo
+		let fetchJournalInfo = new Promise(resolve => {
+			let numJournalResults = 6;
+			base('journal results').select({
+			    // Selecting the first 3 records in Grid view:
+			    maxRecords: numJournalResults, 
+			    view: "Grid view"
+			}).eachPage(function page(records, fetchNextPage) {
+			    // This function (`page`) will get called for each page of records.
+
+			    records.forEach(function(record) {
+			        journalInfo.push({
+			        	"frog": record.get('Frog'),
+			        	"description": record.get('Description'),
+			        	"step1image": record.get('Step 1 Image'),
+			        	"step2image": record.get('Step 2 Image'),
+			        	"step3image": record.get('Step 3 Image'),
+			        	"step4image": record.get('Step 4 Image'),
+			        	"step5image": record.get('Step 5 Image'),
+			        	"step6image": record.get('Step 6 Image'),
+			        	"fullanimation": record.get('Full Animation')
+			        });
+			    });
+
+			    // To fetch the next page of records, call `fetchNextPage`.
+			    // If there are more records, `page` will get called again.
+			    // If there are no more records, `done` will get called.
+			    fetchNextPage();
+
+			}, function done(err) {
+			    if (err) { 
+			    	console.error(err); return; 
+				} else {
+			        resolve(journalInfo);
+				}
+			});
+		});
+
+		// Grab all the journal prompts and save it into journalPrompts
+		let fetchJournalPrompts = new Promise(resolve => {
+			let numJournalPrompts = 30;
+			base('journal prompts').select({
+		    // Selecting the first 3 records in Grid view:
+			    maxRecords: numJournalPrompts,
+			    view: "Grid view"
+			}).eachPage(function page(records, fetchNextPage) {
+			    // This function (`page`) will get called for each page of records.
+
+			    records.forEach(function(record) {
+			        journalPrompts.push(record.get('Prompts'));
+			    });
+
+			    // To fetch the next page of records, call `fetchNextPage`.
+			    // If there are more records, `page` will get called again.
+			    // If there are no more records, `done` will get called.
+			    fetchNextPage();
+
+			}, function done(err) {
+			    if (err) { 
+			    	console.error(err); return; 
+			    } else {
+			        resolve(journalPrompts);
+			    }
+			});
+		});
+
+		// Grab all the frog facts and save it into frogFacts
+		let fetchFrogFacts = new Promise(resolve => {
+			let numFrogFacts = 6;
+			let frogFacts = [];
+			base('frog facts').select({
+		    // Selecting the first 3 records in Grid view:
+			    maxRecords: numFrogFacts,
+			    view: "Grid view"
+			}).eachPage(function page(records, fetchNextPage) {
+			    // This function (`page`) will get called for each page of records.
+
+			    records.forEach(function(record) {
+			        frogFacts.push(record.get('Frog Fact By Step'));
+			    });
+
+			    // To fetch the next page of records, call `fetchNextPage`.
+			    // If there are more records, `page` will get called again.
+			    // If there are no more records, `done` will get called.
+			    fetchNextPage();
+
+			}, function done(err) {
+			    if (err) { 
+			    	console.error(err); return; 
+			    } else {
+			    	resolve(frogFacts);
+			    }
+			});
+		});
+
+		let data = Promise.all([
+			Promise.resolve(fetchQuizInfo),
+			Promise.resolve(fetchJournalInfo),
+			Promise.resolve(fetchJournalPrompts),
+			Promise.resolve(fetchFrogFacts),
+			]).then(results => {
+				let data = {
+					"quizInfo":results[0],
+					"journalInfo":results[1],
+					"journalPrompts":results[2],
+					"frogFacts":results[3]
+				};
+				callback(data);
+			});
+		});
+}
+
+function createQuiz(data) {
     let frogScores = {
         "tomato": 0,
         "milk": 0,
@@ -7,15 +177,47 @@ window.addEventListener("load", (e) => {
         "blue": 0,
         "red": 0,
         "glass": 0
-    }
+    };
 
     let frogNames = {
         "tomato": "Tomato Frog",
         "milk": "Milk Frog",
         "mimic": "Mimic Frog",
         "blue": "Blue Poison Dart Frog",
-        "red": "Red Eyes Tree Frog",
+        "red": "Red Eyed Tree Frog",
         "glass": "Glass Frog"
+    };
+
+    // Restructure journal prompts from Airtable data
+	let prompts = data["journalPrompts"];
+
+	// Restructure frog facts from Airtable data
+	let frogFacts = {};
+	data["frogFacts"].forEach((f,i) => {
+		frogFacts[i+1] = f;
+	});
+
+	// Restructure all the step-by-step frog pics from Airtable data
+    let allFrogImgs = {};
+    data["journalInfo"].forEach(i => {
+    	let frogName = i["frog"];
+    	allFrogImgs[frogName] = {
+    		1:i["step1image"][0]["url"],
+    		2:i["step2image"][0]["url"],
+    		3:i["step3image"][0]["url"],
+    		4:i["step4image"][0]["url"],
+    		5:i["step5image"][0]["url"],
+    		6:i["step6image"][0]["url"],
+    	}
+    });
+    let frogImgs = 8;
+
+    function fetchLocalStorage(key) {
+        if (localStorage.getItem(key) === null) {
+            return "";
+        } else {
+            return localStorage.getItem(key);
+        };
     }
 
     let quizContainer = document.getElementById("quiz-container");
@@ -52,12 +254,91 @@ window.addEventListener("load", (e) => {
     resetButton.addEventListener("click", function() {
         reset();
     });
+
+    function getCurrFrogDesc() {
+    	let frogDesc;
+    	data["journalInfo"].forEach(f => {
+			if (f["frog"] == fetchLocalStorage("frogType")) {
+				frogDesc = f["description"];
+			}
+		});
+		return frogDesc;
+    }
+
+    ///////////////////////////////////////////////////////////
+    //
+    // Generating the frog quiz using AirTable info
+    //
+    ///////////////////////////////////////////////////////////
+
+    // Go through each of the quiz questions
+	data["quizInfo"].forEach((q,i) => {
+		// create a 1-indexed counter
+		let n = i+1;
+		// Create a div to hold them all
+		let qDiv = document.createElement('div');
+		qDiv.classList.add("frog-q","q");
+		qDiv.id = "q" + n;
+		// Make the question a heading
+		let qH1 = document.createElement('h1');
+		qH1.innerHTML = q["question"];
+		qDiv.append(qH1);
+
+		let j = 1;
+		let qAnswers = [];
+		while (j <= 6) {
+			if (q['answer' + j] != undefined) {
+				// And make the answers labeled radio buttons
+				let qLabel = document.createElement("label");
+				qLabel.setAttribute("for","q" + j);
+				qLabel.innerHTML = q['answer' + j];
+
+				let qRadio = document.createElement("input");
+				qRadio.setAttribute("type","radio");
+				qRadio.setAttribute("name","q" + j);
+				// Add all the classes to the radio input for scoring
+				qRadio.classList.add("frog");
+				q['frog'+j].split(" ").forEach(c => {
+					qRadio.classList.add(c);
+				});
+				qLabel.prepend(qRadio);
+				qAnswers.push(qLabel);
+			}
+			qAnswers.forEach(a => qDiv.append(a))
+			j++;
+		}
+		quizContainer.append(qDiv);
+	});
+
+	let qButton = document.createElement("button");
+	qButton.setAttribute("value","submit");
+	qButton.id = "quiz-button";
+	qButton.innerHTML = "submit";
+	quizContainer.append(qButton);
+
+	///////////////////////////////////////////////////////////
+    //
+    // Updating the journal with quiz results + Airtable info
+    //
+    ///////////////////////////////////////////////////////////
+
+    // A function that will update the journal when a new prompt response is submitted and when the page is reloaded
+    function updateJournalPage(cycleStep) {
+    	document.getElementById("prompt").innerHTML = prompts[eval(cycleStep)-1];
+    	frogImgs = allFrogImgs[fetchLocalStorage("frogType")];
+        document.getElementById("frog-img").src = eval(cycleStep) <= 6 ? frogImgs[eval(cycleStep)] : frogImgs[6];
+
+        document.getElementById("frog-fact").innerHTML = eval(cycleStep) <= 6 ? frogFacts[eval(cycleStep)] : getCurrFrogDesc();
+    }
+
+    // With all that data available, we're now going to start effectively saving state
     // If there's no info in localStorage
     if (localStorage.length == 0) {
         // Then reset the app
         reset();
-        // If there is info in localStorage
+    // If there is info in localStorage
     } else {
+    	// This means the quiz has already been filled out
         // Show the journal
         quizContainer.classList.add("invisible");
         journalContainer.classList.remove("invisible");
@@ -66,35 +347,12 @@ window.addEventListener("load", (e) => {
         // Their journal entries
         document.getElementById("entries").innerHTML = fetchLocalStorage("journal");
         // And the correct journal prompt and frog image for the cycle step they left off at
-
-    }
-    ///////////////////////////////////////////////////////////
-    //
-    // Journaling to evolve your frog
-    //
-    ///////////////////////////////////////////////////////////
-
-    // Assuming we want to loop questions, there should be the same number of frog images as there are questions. If we decide not to loop, code needs to be rewritten and there should be one more, ending with a landing page with full-grown frog. 
-    let frogImgs = {
-        1: "https://media.istockphoto.com/id/172191851/photo/frog-spawn-in-a-puddle-next-to-a-trail.jpg?s=612x612&w=0&k=20&c=VxPQjoMS7MxWRdwRuL8pXoySAYRauWgsyw81wf8qLuk=",
-        2: "https://cdn.britannica.com/20/231520-050-9DF96017/tadpole-common-frog-amphibian.jpg",
-        3: "https://i.natgeofe.com/k/8fa25ea4-6409-47fb-b3cc-4af8e0dc9616/red-eyed-tree-frog-on-leaves-3-2_3x2.jpg",
-        // 4: "https://www.timeforkids.com/wp-content/uploads/2018/03/frog2.jpg?w=1024" 
+        updateJournalPage(fetchLocalStorage("cycleStep"));
     }
 
-    // Grab all the questions from the page
-    let qs = document.getElementsByClassName("journal-q");
-
-    function fetchLocalStorage(key) {
-        if (localStorage.getItem(key) === null) {
-            return "";
-        } else {
-            return localStorage.getItem(key);
-        };
-    }
     ///////////////////////////////////////////////////////////
     //
-    // Which frog are you quiz
+    // Whenever the quiz submit button is pressed
     //
     ///////////////////////////////////////////////////////////
 
@@ -123,66 +381,72 @@ window.addEventListener("load", (e) => {
         localStorage.setItem("frogType", frogType);
         // And set the title of the journal page to reflect the frog type
         document.getElementById("frogType").innerHTML = frogNames[frogType];
-
+        // Set the cycleStep to 1
+        localStorage.setItem("cycleStep","1")
+        // Update the journal page with the current frog image, fact, and journal prompt
+        updateJournalPage(fetchLocalStorage("cycleStep"));		
+        // And continue to update the journal whenever the page is reloaded
+  		window.addEventListener("load", (event) => {
+			updateJournalPage(fetchLocalStorage("cycleStep"));
+		});
         // Finally, hide the quiz and show the journal
         quizContainer.classList.toggle("invisible");
         journalContainer.classList.toggle("invisible");
     });
 
-    // For each question
-    Array.from(qs).forEach((q, i) => {
-        // Grab its button
-        let button = q.childNodes[5];
-        // And generate its index number, starting with 1
-        let n = i + 1;
-        // Add a click-based event listener to the button
-        button.addEventListener("click", function(b) {
-            // If there is content in the button's associated text area
-            if (b.srcElement.previousElementSibling.value != "") {
-                // Get the current date and time and use it to create a log entry title 
-                let date = new Date(Date.now()).toLocaleString();
-                let entryTitle = document.createElement("h2");
-                entryTitle.innerHTML = date;
+	///////////////////////////////////////////////////////////
+    //
+    // Whenever the journal submit button is pressed
+    //
+    ///////////////////////////////////////////////////////////
 
-                // Grab the current textarea input and use it to create a log entry, and then delete the input so it will be empty next time   
-                let content = b.srcElement.previousElementSibling.value;
-                b.srcElement.previousElementSibling.value = "";
-                let entryContent = document.createElement("p");
-                entryContent.classList.add("entry");
-                entryContent.innerHTML = content;
+    let frogImgDiv = document.getElementById("frog-img");
+    let frogFactDiv = document.getElementById("frog-fact");
 
-                // Store the date into local storage by appending to a string we can decode when you return to the page, so the current state can persist
-                let journalStorage = fetchLocalStorage("journal");
-                let newJournalStorageEntry = entryTitle.outerHTML + entryContent.outerHTML;
-                localStorage.setItem("journal", newJournalStorageEntry + journalStorage);
+    let journalButton = document.getElementById("journal-button");
+    journalButton.addEventListener("click", function(b) {
+        // If there is content in the button's associated text area
+        if (b.srcElement.previousElementSibling.value != "") {
+            // Get the current date and time and use it to create a log entry title 
+            let date = new Date(Date.now()).toLocaleString();
+            let entryTitle = document.createElement("h2");
+            entryTitle.innerHTML = date;
 
-                // While we're saving state, also log which step in the frog evolution cycle we're on so we can return to it
-                localStorage.setItem("cycleStep", b.srcElement.parentNode.id);
+            // Grab the current textarea input and use it to create a log entry, and then delete the input so it will be empty next time   
+            let content = b.srcElement.previousElementSibling.value;
+            b.srcElement.previousElementSibling.value = "";
+            let entryContent = document.createElement("p");
+            entryContent.classList.add("entry");
+            entryContent.innerHTML = content;
 
-                //Then grab the whole log and prepend this new entry
-                let log = document.getElementById("entries");
-                let entry = document.createElement("div");
-                entry.classList.add("entry");
-                entry.append(entryTitle)
-                entry.append(entryContent);
-                log.prepend(entry);
+            // Store the date into local storage by appending to a string we can decode when you return to the page, so the current state can persist
+            let journalStorage = fetchLocalStorage("journal");
+            let newJournalStorageEntry = entryTitle.outerHTML + entryContent.outerHTML;
+            localStorage.setItem("journal", newJournalStorageEntry + journalStorage);
 
-                // Next hide the current question
-                document.getElementById(n).classList.toggle("invisible");
-                // If there are more questions in the questions list
-                if (n + 1 <= qs.length) {
-                    // Then show the next question and progress to the next frog image
-                    document.getElementById(n + 1).classList.toggle("invisible");
-                    document.getElementById("frog-img").src = frogImgs[n + 1];
-                    // If we're at the last question in the list
-                } else {
-                    // Then loop back around to the first question and frog image
-                    document.getElementById(1).classList.toggle("invisible");
-                    document.getElementById("frog-img").src = frogImgs[1];
-                }
-            };
+            // While we're saving state, also log which step in the frog evolution cycle we're on so we can return to it
+            localStorage.setItem("cycleStep", eval(fetchLocalStorage("cycleStep")) + 1);
 
-            console.log("local storage for journal key is now: " + localStorage.getItem("journal"))
-        });
-    })
-});
+            //Then grab the whole log and prepend this new entry
+            let log = document.getElementById("entries");
+            let entry = document.createElement("div");
+            entry.classList.add("entry");
+            entry.append(entryTitle)
+            entry.append(entryContent);
+            log.prepend(entry);
+
+            // Progress to the next frog image + cycleFact, until you reach the last frog
+            if (eval(fetchLocalStorage("cycleStep")) < 6) {
+            	frogImgDiv.src = frogImgs[eval(fetchLocalStorage("cycleStep"))];
+            	frogFactDiv.innerHTML = frogFacts[eval(fetchLocalStorage("cycleStep"))];
+            } else {
+            	frogImgDiv.src = frogImgs[6];
+            	let frogDesc; 
+            	frogFactDiv.innerHTML = getCurrFrogDesc();
+            }
+        };
+
+    }); 
+}
+
+fetchData(createQuiz);
